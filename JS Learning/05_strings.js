51f9804a9ce20e4b541ac4ccf8b5344cdb64e096
Pratham-x-14 <shah.x014@gmail.com>
2026-07-22 22:14:14 +0530

    string learning update

--- JS Learning/05_strings.js
+++ JS Learning/05_strings.js
@@ -6,25 +6,20 @@ const repoCount = 25;
 console.log(`Hello my name is ${name} and my repo count is 
     ${repoCount} `);
 
-    let oldGameName1 = new String("gta");
-    oldGameName = oldGameName1
-    newGameName = "GTA5";
-    // console.log(gameName[2])
-    // console.log(gameName.__proto__); /*__proto__ is used to 
-    // access the prototype of an object in JavaScript. 
-    // In this case, it will show the prototype of the String 
-    // object, which includes various methods and properties 
-    // that can be used with string instances.*/
+let oldGameName1 = new String("gta");
+oldGameName = oldGameName1;
+newGameName = "GTA5";
+console.log(newGameName[2]);
+console.log(newGameName.__proto__); /*__proto__ is used to 
+    access the prototype of an object in JavaScript. 
+    In this case, it will show the prototype of the String 
+    object, which includes various methods and properties 
+    that can be used with string instances.*/
 
-    //console.log(gameName.length);
-    console.log(oldGameName);
-    console.log(oldGameName.toString());
-    console.log(newGameName);
-    console.log(newGameName.charAt(2));
-    console.log(oldGameName.charAt(2));
-    
-    
-
-    
-
-
+console.log(newGameName.length);
+console.log(oldGameName);
+console.log(oldGameName.toString());
+console.log(newGameName);
+console.log(newGameName.charAt(2));
+console.log(oldGameName.charAt(2));
+console.log(newGameName.indexOf('T'));
